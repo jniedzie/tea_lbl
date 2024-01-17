261cbbbb39be0c93b0724d9aa6ff3b0a197b9d37
Jeremi Niedziela <jeremi.niedziela@cern.ch>
2024-01-17 08:22:10 +0100

    Updating skimmer.

--- apps/lbl_skimmer.cpp
+++ apps/lbl_skimmer.cpp
@@ -1,12 +1,12 @@
 #include "ConfigManager.hpp"
 #include "CutFlowManager.hpp"
+#include "EventProcessor.hpp"
 #include "EventReader.hpp"
 #include "EventWriter.hpp"
 #include "ExtensionsHelpers.hpp"
+#include "HistogramsFiller.hpp"
 #include "HistogramsHandler.hpp"
 #include "Profiler.hpp"
-#include "HistogramsFiller.hpp"
-#include "EventProcessor.hpp"
 
 // If you also created a histogram filler, you can include it here
 // #include "MyHistogramsFiller.hpp"
@@ -15,23 +15,23 @@ using namespace std;
 
 void CheckArgs(int argc, char **argv) {
   if (argc != 2 && argc != 4) {
-    fatal() << "Usage: " << argv[0] << " config_path"<<endl;
-    fatal() << "or"<<endl;
-    fatal() << argv[0] << " config_path input_path output_path"<<endl;
+    fatal() << "Usage: " << argv[0] << " config_path" << endl;
+    fatal() << "or" << endl;
+    fatal() << argv[0] << " config_path input_path output_path" << endl;
     exit(1);
   }
 }
 
 int main(int argc, char **argv) {
   CheckArgs(argc, argv);
-  
+
   // Initialize ConfigManager with the path passed as an argument to the app
   ConfigManager::Initialize(argv[1]);
 
   auto &config = ConfigManager::GetInstance();
 
-  // If you want to override input/output paths, you can do it here  
-  if(argc == 4){    
+  // If you want to override input/output paths, you can do it here
+  if (argc == 4) {
     config.SetInputPath(argv[2]);
     config.SetOutputPath(argv[3]);
   }
@@ -44,14 +44,14 @@ int main(int argc, char **argv) {
   // Create a CutFlowManager to keep track of how many events passed selections
   auto cutFlowManager = make_shared<CutFlowManager>(eventReader, eventWriter);
 
- 
   cutFlowManager->RegisterCut("initial");
   // cutFlowManager->RegisterCut("trigger");
-  cutFlowManager->RegisterCut("nPassingEBtower");
+  // cutFlowManager->RegisterCut("nPassingEBtower");
   cutFlowManager->RegisterCut("nPassingTower");
+  cutFlowManager->RegisterCut("nPassingZDCcounts");
 
-  string eventsTreeName;
-  config.GetValue("eventsTreeName", eventsTreeName);
+  vector<string> eventsTreeNames;
+  config.GetVector("eventsTreeNames", eventsTreeNames);
 
   // Start the event loop
   for (int iEvent = 0; iEvent < eventReader->GetNevents(); iEvent++) {
@@ -66,8 +66,17 @@ int main(int argc, char **argv) {
 
     // auto towersAboveThreshold = event->GetCollection("PassingTower");
     // for(auto tower : *towersAboveThreshold){
-    //   info() << "Tower energy: " << (float)tower->Get("et") << "\teta: " << (float)tower->Get("eta") << "\toriginal collection: " << tower->GetOriginalCollection() << endl;
+    //   info() << "Tower energy: " << (float)tower->Get("et") << "\teta: " << (float)tower->Get("eta") << "\toriginal collection: " <<
+    //   tower->GetOriginalCollection() << endl;
     // }
+
+    auto photons = event->GetCollection("photon");
+    for(auto photon : *photons){
+      info() << "Photon energy: " << (float)photon->Get("et") << "\teta:" << (float)photon->Get("eta") << endl;
+    }
+
+    // auto goodPhotons = event->GetCollection("GoodPhotons");
+    // info() << "Found " << goodPhotons->size() << " good photons" << endl;
 
     cutFlowManager->UpdateCutFlow("initial");
 
@@ -75,10 +84,12 @@ int main(int argc, char **argv) {
     // if(!passesTrigger) continue;
     // cutFlowManager->UpdateCutFlow("trigger");
 
-    if(!eventProcessor->PassesEventSelections(event, cutFlowManager)) continue;
+    if (!eventProcessor->PassesEventSelections(event, cutFlowManager)) continue;
 
     // If you want to store this event in the output tree, add it to the eventWriter
-    eventWriter->AddCurrentEvent(eventsTreeName);
+    for (string eventsTreeName : eventsTreeNames) {
+      eventWriter->AddCurrentEvent(eventsTreeName);
+    }
   }
 
   // Tell CutFlowManager to save the cut flow
--- configs/lbl_skimmer_config.py
+++ configs/lbl_skimmer_config.py
@@ -1,12 +1,15 @@
 ## specify how many events to run on (and how often to print current event number)
-nEvents = 100
-printEveryNevents = 10
+nEvents = 10
+printEveryNevents = 1
 
-inputFilePath = "/eos/cms/store/cmst3/group/lightbylight/Pranati/HIForest_LbyL_Signal_MC/LbyLSignal_5p02TeV_SuperChic/reco_mc_lbl_try2/230821_061454/0000/HiForest_SC_LbL_Merged.root"
+# inputFilePath = "/eos/cms/store/cmst3/group/lightbylight/Pranati/Final_afterTrigger/Data/Data_29thJune/merged/merged_ntuples_2.root"
+inputFilePath = "./test_renamed.root"
 treeOutputFilePath = "./test_skimmed.root"
 
 weightsBranchName = "genWeight"
-eventsTreeName = "ggHiNtuplizer/EventTree"
+eventsTreeNames = [
+    "Events",
+]
 
 # define extra collections:
 # - give it a name: e.g. GoodLeptons
@@ -15,39 +18,67 @@ eventsTreeName = "ggHiNtuplizer/EventTree"
 # you can specify a range for this parameter
 extraEventCollections = {
     # EM calo towers
-    "PassingEBtower":       {"inputCollections": ("CaloTower",), "eta": (-1.4442 , 1.4442), "et": (0.7, 9999999.)},
-    "PassingEEtowerPlus":   {"inputCollections": ("CaloTower",), "eta": (1.566   , 2.6   ), "et": (3.0, 9999999.)},
-    "PassingEEtowerMinus":  {"inputCollections": ("CaloTower",), "eta": (-2.6    , 1.566 ), "et": (3.0, 9999999.)},
+    # "PassingEBtower":       {"inputCollections": ("CaloTower",), "eta": (-1.4442 , 1.4442), "et": (0.7, 9999999.)},
+    # "PassingEEtowerPlus":   {"inputCollections": ("CaloTower",), "eta": (1.566   , 2.6   ), "et": (3.0, 9999999.)},
+    # "PassingEEtowerMinus":  {"inputCollections": ("CaloTower",), "eta": (-2.6    , 1.566 ), "et": (3.0, 9999999.)},
     
-    # Had calo towers
-    "PassingHBtower":       {"inputCollections": ("CaloTower",), "eta": (-1.305  , 1.305 ), "et": (2.8, 9999999.)},
-    "PassingHEtowerPlus":   {"inputCollections": ("CaloTower",), "eta": (1.41    , 3.0   ), "et": (1.0, 9999999.)},
-    "PassingHEtowerMinus":  {"inputCollections": ("CaloTower",), "eta": (-3.0    , 1.41  ), "et": (1.0, 9999999.)},
+    # # Had calo towers
+    # "PassingHBtower":       {"inputCollections": ("CaloTower",), "eta": (-1.305  , 1.305 ), "et": (2.8, 9999999.)},
+    # "PassingHEtowerPlus":   {"inputCollections": ("CaloTower",), "eta": (1.41    , 3.0   ), "et": (1.0, 9999999.)},
+    # "PassingHEtowerMinus":  {"inputCollections": ("CaloTower",), "eta": (-3.0    , 1.41  ), "et": (1.0, 9999999.)},
     
-    # Forward calo towers
-    "PassingHFtowerPlus":   {"inputCollections": ("CaloTower",), "eta": (3.15    , 5.2   ), "et": (6.0, 9999999.)},
-    "PassingHFtowerMinus":  {"inputCollections": ("CaloTower",), "eta": (-5.2    , 3.15  ), "et": (6.0, 9999999.)},
+    # # Forward calo towers
+    # "PassingHFtowerPlus":   {"inputCollections": ("CaloTower",), "eta": (3.15    , 5.2   ), "et": (6.0, 9999999.)},
+    # "PassingHFtowerMinus":  {"inputCollections": ("CaloTower",), "eta": (-5.2    , 3.15  ), "et": (6.0, 9999999.)},
     
-    "PassingTower":  {"inputCollections": (
-        "PassingEBtower", 
-        "PassingEEtowerPlus", 
-        "PassingEEtowerMinus",
-        "PassingHBtower",
-        "PassingHEtowerPlus",
-        "PassingHEtowerMinus",
-        "PassingHFtowerPlus",
-        "PassingHFtowerMinus",)
+    # "PassingTower":  {"inputCollections": (
+    #     "PassingEBtower", 
+    #     "PassingEEtowerPlus", 
+    #     "PassingEEtowerMinus",
+    #     "PassingHBtower",
+    #     "PassingHEtowerPlus",
+    #     "PassingHEtowerMinus",
+    #     "PassingHFtowerPlus",
+    #     "PassingHFtowerMinus",)
+    # },
+    
+    "PassingZDCcounts":  {"inputCollections": ("ZDC",), "energy": (10.0, 9999999.)},
+    
+    "GoodBarrelPhotons": {
+        "inputCollections": ("photon",), 
+        "et": (2.0, 9999999.), 
+        "eta": (-2.2, 2.2),
     },
+    
+    # photonMinEt: 2.0 
+    # photonMaxEta: 2.2 
+    # photonMaxHoverEbarrel: 0.04596 
+    # photonMaxHoverEendcap: 0.0590 
+    # photonMaxEtaWidthBarrel: 0.0106 
+    # photonMaxEtaWidthEndcap: 0.0272  
+    # photonMinSwissCross: 999
+    # photonMaxSwissCross: 0.95
+    # diphotonMaxPt: 1.0 
+    # diphotonMaxRapidity: 9999999 
+    # diphotonMinMass: 5.0 
+    # diphotonMaxAco: 0.01 
+    # photonRejectConverted: 1
+    
 }
 
 # define simple event-level selections
 eventSelections = {
-    "nPassingEBtower": (1, 9999999),
-    "nPassingTower": (0, 0),
+    # "nPassingTower": (0, 0),
+    "nPassingZDCcounts": (0, 0),
 }
 
 specialBranchSizes = {
     "Particle": "Event_numberP",
+    "e": "n",
+    "channel": "n",
+    "saturation": "n",
+    "section": "n",
+    "zside": "n",
 }
 
 branchesToKeep = ["*"]
