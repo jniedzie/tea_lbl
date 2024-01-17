3cd89857d4729ccb8cc95c0807ff76ebf96b0195
Jeremi Niedziela <jeremi.niedziela@cern.ch>
2024-01-17 08:22:23 +0100

    Adding a class for LbL selections.

---
+++ libs/user_extensions/include/LbLSelections.hpp
@@ -0,0 +1,14 @@
+class LbLSelections {
+public:
+  LbLSelections() = default;
+  ~LbLSelections() = default;
+
+  // LbLSelections(const LbLSelections& other) = delete;
+  // LbLSelections(LbLSelections&& other) = delete;
+  // LbLSelections& operator=(const LbLSelections& other) = delete;
+  // LbLSelections& operator=(LbLSelections&& other) = delete;
+
+  // bool operator==(const LbLSelections& other) const;
+  // bool operator!=(const LbLSelections& other) const;
+
+};
