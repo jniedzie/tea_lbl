1dc822e1ae8070844923725ca02deab20fbf8dc1
Jeremi Niedziela <jeremi.niedziela@cern.ch>
2024-04-09 10:28:04 +0200

    Refactoring script preparing unfolding histograms. Implementing poisson errors and corresponding scaling.

--- utils/lbl_scale_unfolding_histograms.py
+++ utils/lbl_scale_unfolding_histograms.py
@@ -6,86 +6,71 @@ from lbl_paths import merged_histograms_path, processes, skim, acoplanarity_hist
 
 samples = processes
 
+
 def main():
     files = {}
+    hists = {}
+    hists_up = {}
+    hists_low = {}
+    variables = ["mass", "pt", "absRap", "absRap3", "rap3", "rap4",
+                 "costhetastar2", "costhetastar3", "costhetastar4", "aco"]
 
-    hists_mass = {}
-    hists_pt = {}
-    hists_absrap = {}
-    hists_absrap3 = {}
-    
-    
-    hists_rap3 = {}
-    hists_rap4 = {}
-    hists_costheta2 = {}
-    hists_costheta3 = {}
-    hists_costheta4 = {}
-    
-    hists_aco = {}
+    for variable in variables:
+        hists[variable] = {}
+        hists_up[variable] = {}
+        hists_low[variable] = {}
 
     photon_sf, _ = get_scale_factor(photon=True)
 
     for sample in samples:
-        files[sample] = ROOT.TFile.Open(merged_histograms_path.format(sample, skim))
+        print(
+            f"Processing file: {merged_histograms_path.format(sample, skim)}")
+        files[sample] = ROOT.TFile.Open(
+            merged_histograms_path.format(sample, skim))
+        output_file = ROOT.TFile(
+            f"unfoldingHistograms_{sample}.root", "recreate")
 
-        hists_mass[sample] = files[sample].Get("unfoldingPhoton_mass")
-        hists_pt[sample] = files[sample].Get("unfoldingPhoton_pt")
-        hists_absrap[sample] = files[sample].Get("unfoldingPhoton_absRap")
-        hists_absrap3[sample] = files[sample].Get("unfoldingPhoton_absRap3")
-        hists_rap3[sample] = files[sample].Get("unfoldingPhoton_rap3")
-        hists_rap4[sample] = files[sample].Get("unfoldingPhoton_rap4")
-        hists_costheta2[sample] = files[sample].Get("unfoldingPhoton_costhetastar2")
-        hists_costheta3[sample] = files[sample].Get("unfoldingPhoton_costhetastar3")
-        hists_costheta4[sample] = files[sample].Get("unfoldingPhoton_costhetastar4")
-        
-        hists_aco[sample] = files[sample].Get(acoplanarity_histogram_name.format(n_acoplanarity_bins))
+        for variable in variables:
+            if variable == "aco":
+                name = acoplanarity_histogram_name.format(n_acoplanarity_bins)
+            else:
+                name = f"unfoldingPhoton_{variable}"
 
-        if sample == "collisionData":
-            pass
-        elif sample == "lbl" or "qed" in sample:
-            hists_mass[sample].Scale(
-                luminosity*crossSections[sample]*photon_sf/nGenEvents[sample])
-            hists_pt[sample].Scale(
-                luminosity*crossSections[sample]*photon_sf/nGenEvents[sample])
-            hists_absrap[sample].Scale(luminosity*crossSections[sample]*photon_sf/nGenEvents[sample])
-            hists_absrap3[sample].Scale(luminosity*crossSections[sample]*photon_sf/nGenEvents[sample])
-            hists_rap3[sample].Scale(
-                luminosity*crossSections[sample]*photon_sf/nGenEvents[sample])
-            hists_rap4[sample].Scale(
-                luminosity*crossSections[sample]*photon_sf/nGenEvents[sample])
-            
-            hists_costheta2[sample].Scale(luminosity*crossSections[sample]*photon_sf/nGenEvents[sample])
-            hists_costheta3[sample].Scale(luminosity*crossSections[sample]*photon_sf/nGenEvents[sample])
-            hists_costheta4[sample].Scale(luminosity*crossSections[sample]*photon_sf/nGenEvents[sample])
-            
-            hists_aco[sample].Scale(luminosity*crossSections[sample]*photon_sf/nGenEvents[sample])
-            
-        elif sample == "cep":
-            cep_scale = get_cep_scale(skim)[0]
-            hists_mass[sample].Scale(cep_scale)
-            hists_pt[sample].Scale(cep_scale)
-            hists_absrap[sample].Scale(cep_scale)
-            hists_absrap3[sample].Scale(cep_scale)
-            hists_rap3[sample].Scale(cep_scale)
-            hists_rap4[sample].Scale(cep_scale)
-            hists_costheta2[sample].Scale(cep_scale)
-            hists_costheta3[sample].Scale(cep_scale)
-            hists_costheta4[sample].Scale(cep_scale)
-            hists_aco[sample].Scale(cep_scale)
-            
-        output_file = ROOT.TFile(f"unfoldingHistograms_{sample}.root", "recreate")
-        output_file.cd()
-        hists_mass[sample].Write()
-        hists_pt[sample].Write()
-        hists_absrap[sample].Write()
-        hists_absrap3[sample].Write()
-        hists_rap3[sample].Write()
-        hists_rap4[sample].Write()
-        hists_costheta2[sample].Write()
-        hists_costheta3[sample].Write()
-        hists_costheta4[sample].Write()
-        hists_aco[sample].Write()
-        
+            hists[variable][sample] = files[sample].Get(name)
+            hists[variable][sample].SetBinErrorOption(ROOT.TH1.kPoisson)
+
+            err_low = {}
+            err_up = {}
+
+            for i in range(1, hists[variable][sample].GetNbinsX()+1):
+                err_low[i] = hists[variable][sample].GetBinErrorLow(i)
+                err_up[i] = hists[variable][sample].GetBinErrorUp(i)
+
+            if sample == "collisionData":
+                scale = 1
+            elif sample == "lbl" or "qed" in sample:
+                scale = luminosity * \
+                    crossSections[sample]*photon_sf/nGenEvents[sample]
+            elif sample == "cep":
+                scale = get_cep_scale(skim)[0]
+
+            hists[variable][sample].Scale(scale)
+
+            hists_up[variable][sample] = hists[variable][sample].Clone()
+            hists_low[variable][sample] = hists[variable][sample].Clone()
+
+            hists_up[variable][sample].SetName(name+"_errUp")
+            hists_low[variable][sample].SetName(name+"_errLow")
+
+            for i in range(1, hists[variable][sample].GetNbinsX()+1):
+                hists_low[variable][sample].SetBinError(i, err_low[i]*scale)
+                hists_up[variable][sample].SetBinError(i, err_up[i]*scale)
+
+            output_file.cd()
+            hists[variable][sample].Write()
+            hists_low[variable][sample].Write()
+            hists_up[variable][sample].Write()
+
         output_file.Close()
 
 
